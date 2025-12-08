8270d2053f60e411c237d8ef7741b275541e629a
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2025-12-08 20:25:01 +0300

    Add files via upload

---
+++ empty_folder/install_requirements.bat
@@ -0,0 +1,6 @@
+@echo Hello, starting install
+pip install -r requirements.txt
+@echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+@echo If installion failed - install python
+@echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+pause
