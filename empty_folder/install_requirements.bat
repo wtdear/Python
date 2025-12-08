9af9df1305885cbd8484482924e66c0a2033de95
Johhny Kolbaya <148680773+wtdear@users.noreply.github.com>
2025-12-08 20:27:27 +0300

    Delete empty_folder directory

--- empty_folder/install_requirements.bat
+++
@@ -1,6 +0,0 @@
-@echo Hello, starting install
-pip install -r requirements.txt
-@echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-@echo If installion failed - install python
-@echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-pause
